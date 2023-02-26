4d846678f9787dc9b0f5d308f206fe723b374385
Prilovsky Viktor <112697876+vainarito@users.noreply.github.com>
2023-02-26 20:03:56 +0300

    Add files via upload

---
+++ word substitution/src/file/si.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char* createFile();
+void fillFile(char*);
+void parseStr(char*, const char*, const char*);
+int numStrInFile(const char*);
+void fillDictionary(char*);
